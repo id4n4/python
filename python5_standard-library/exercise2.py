9a1e29963f7a63c7348ebb4de2f85fb7be686744
id4n4 <nagatiitoo@hotmail.com>
2022-01-11 09:05:18 -0500

    Python 5 - Update

--- python5_standard-library/exercise2.py
+++ python5_standard-library/exercise2.py
@@ -1,6 +1,7 @@
 #py -m pip install emoji
 # La utilidad pip instala los paquetes y las dependencias que se enumeran en el
 # índice de paquetes de Python (PyPI)
-import emoji
-message = emoji.emojize('Howdy :sun_with_face:')
-print(message)
+
+# import emoji
+# message = emoji.emojize('Howdy :sun_with_face:')
+# print(message)
